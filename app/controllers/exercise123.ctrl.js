052775ad7fadcc07471ce2ad37f4058e755cfa49
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-09-11 19:10:04 -0500

    got a timer to work

--- app/controllers/exercise123.ctrl.js
+++ app/controllers/exercise123.ctrl.js
@@ -1,5 +1,54 @@
 'use strict';
 
-app.controller("exerciseCtrl", function($scope, $routeParams){
+app.controller("exerciseCtrl", function($scope, $routeParams, $interval, $timeout){
     
-    });
+    $scope.timerWithTimeout = 0;
+    $scope.startTimerWithTimeout = function() {
+     $scope.timerWithTimeout = 0;
+     if($scope.myTimeout){
+       $timeout.cancel($scope.myTimeout);
+     }
+     $scope.onTimeout = function(){
+         $scope.timerWithTimeout++;
+         $scope.myTimeout = $timeout($scope.onTimeout);
+     };
+     $scope.myTimeout = $timeout($scope.onTimeout);
+   };
+   
+   $scope.resetTimerWithTimeout = function(){
+     $scope.timerWithTimeout = 0;
+     $timeout.cancel($scope.myTimeout);
+   };
+   
+   //timer with interval
+//    $scope.timerWithInterval = 0;
+    $scope.startTimerWithInterval = function() {
+     $scope.timerWithInterval = 0;
+     if($scope.myInterval){
+       $interval.cancel($scope.myInterval);
+     }
+     $scope.onInterval = function(){
+         $scope.timerWithInterval++;
+     };
+     $scope.myInterval = $interval($scope.onInterval,1000);
+   };
+   
+   $scope.resetTimerWithInterval = function(){
+     $scope.timerWithInterval = 0;
+     $interval.cancel($scope.myInterval);
+   };
+ });
+ app.filter('hhmmss', function () {
+   return function (time) {
+     var sec_num = parseInt(time, 10); // don't forget the second param
+     var hours   = Math.floor(sec_num / 3600);
+     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+     var seconds = sec_num - (hours * 3600) - (minutes * 60);
+ 
+     if (hours   < 10) {hours   = "0"+hours;}
+     if (minutes < 10) {minutes = "0"+minutes;}
+     if (seconds < 10) {seconds = "0"+seconds;}
+     time = hours+':'+minutes+':'+seconds;
+     return time;
+   };
+ });
---
+++ app/values/fb.ctrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.constant("FBCreds", {
+    apiKey: "AIzaSyAZEgBKIJo7cCk6R4lPkc_jfoHjm54qbao",
+    authDomain: "capstone-57cef.firebaseapp.com",
+    databaseURL: "https://capstone-57cef.firebaseio.com"
+});
